fc9eeb4776d21dc2bc8efd77966c04ca94ec789e
matiasjava <matias.vejar@usach.cl>
2025-04-01 23:59:13 -0300

    Configuración de postgres con backend y nueva clase

---
+++ Backend/src/main/java/kartingRM/Backend/Entities/KartEntity.java
@@ -0,0 +1,23 @@
+package kartingRM.Backend.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "karts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class KartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String model;
+    private String status;
+}
